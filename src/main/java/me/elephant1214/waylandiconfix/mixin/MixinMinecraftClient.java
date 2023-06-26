e9e23e85ca564911179a29aebf69e9d5fe4c5c3a
Elephant1214 <nathangetsmail@gmail.com>
2023-06-26 17:05:42 -0600

    Why did this method need to be changed twice within four updates

--- src/main/java/me/elephant1214/waylandiconfix/mixin/MixinMinecraftClient.java
+++ src/main/java/me/elephant1214/waylandiconfix/mixin/MixinMinecraftClient.java
@@ -1,8 +1,9 @@
 package me.elephant1214.waylandiconfix.mixin;
 
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Icons;
 import net.minecraft.client.util.Window;
+import net.minecraft.resource.ResourcePack;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
@@ -10,20 +11,24 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.io.InputStream;
+import java.io.IOException;
 
 @Mixin(MinecraftClient.class)
 public class MixinMinecraftClient {
 	private static final Logger LOGGER = LogManager.getLogger("waylandiconfix");
 
-	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;setIcon(Ljava/io/InputStream;Ljava/io/InputStream;)V"))
-	private void fixSetIcon_WaylandIconFix(Window instance, InputStream icon16, InputStream icon32) {
+	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;setIcon(Lnet/minecraft/resource/ResourcePack;Lnet/minecraft/client/util/Icons;)V"))
+	private void fixSetIcon_WaylandIconFix(Window instance, ResourcePack resourcePack, Icons icons) {
 		if (GLFW.glfwGetPlatform() == GLFW.GLFW_PLATFORM_WAYLAND) {
 			LOGGER.info("Running on Wayland, not setting the window icon");
 		} else {
 			LOGGER.fatal("This mod is meant for users running Wayland who are unable to load into the game. If you " +
 					"don't have this issue or don't even know what Wayland is, remove this mod.");
-			instance.setIcon(icon16, icon32);
+			try {
+				instance.setIcon(resourcePack, icons);
+			} catch (IOException e) {
+				LOGGER.error("Couldn't set icon", e);
+			}
 		}
 	}
 }
